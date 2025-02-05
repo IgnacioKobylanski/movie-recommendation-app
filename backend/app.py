8d85f5594960791053b0587893b0371b38f9b356
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-02-05 09:56:32 -0300

    minor change in app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola, mundo! Esto es Flask."
+    return "probando respuesta"
 
 if __name__ == '__main__':
     app.run(debug=True)
