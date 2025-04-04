c9b3e6b09132c7b8738de35368c91c6a110d7f30
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-04-04 17:33:37 -0300

    logo added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../styles/Header.css';
+import logo from '../assets/images/logoW.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <h1>MochiMovies</h1>
+      <img src={logo} alt="MochiMovies Logo" className="logo" />
+        
       </div>
-      <nav>
+      <h1>MochiMovies</h1>
+      <nav className='header-nav'>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
