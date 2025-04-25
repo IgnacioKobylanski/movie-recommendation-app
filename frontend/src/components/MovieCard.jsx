16d965c65fed74933c150866e7d0c36a750bbea2
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-04-25 19:32:15 -0300

    minor changes in MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -24,28 +24,3 @@ const MovieCard = ({ movieInfo }) => {
 };
 
 export default MovieCard;
-
-
-
-
-/* import React from "react";
-import "../styles/MovieCard.css"
-import { Link } from "react-router-dom";
-
-const MovieCard = ({ movieInfo }) => {
-    const movieId = movieInfo.name.replace(/\s+/g, '-').toLowerCase();
-    return (
-        <Link to={`/movie/${movieId}`} className="movie-card-link">
-        <div className="movie-card-container">
-            <img
-                className="movie-small-image"
-                src={movieInfo.imageUrl}
-                alt={movieInfo.name}
-            />
-            <h3>{movieInfo.name}</h3>
-        </div>
-        </Link>
-    );
-};
-
-export default MovieCard; */
